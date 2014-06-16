a5ee98bfb27c5540ea5fb8e98301245104b168a7
Peter Dunbar <Peter.joseph.dunbar@gmail.com>
2014-06-16 08:50:33 -0400

    changed list model to delete tasks that are related to list

--- app/models/list.rb
+++ app/models/list.rb
@@ -5,4 +5,12 @@ class List < ActiveRecord::Base
   has_many :members
   has_many :users, through: :members
   has_many :tasks
+
+  before_destroy :destroy_tasks
+
+  private
+
+  def :destroy_tasks
+    self.tasks.delete_all
+  end
 end
